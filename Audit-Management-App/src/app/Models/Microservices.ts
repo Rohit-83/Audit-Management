39344cf47b0a2f92c2989be3f987d1a8e957bc48
Ayan <ayanchatterjee987@gmail.com>
2022-07-21 12:59:44 +0530

    login eddited checklist edited

--- Audit-Management-App/src/app/Models/Microservices.ts
+++ Audit-Management-App/src/app/Models/Microservices.ts
@@ -2,7 +2,12 @@ export const Microservices : {[key: string]: string} = {
     // give like this : 
     // "ms-name" : "http://localhost:[port-no.]/[context-root]"
     "auth"      : "http://localhost:8100/auth",
+    
     "checklist" : "http://localhost:8200/checklist",
+    // "checklist" : "AUDUT-CHECKLIST/",
     "benchmark" : "http://localhost:8250/benchmark",
+    // "benchmark" : "AUDIT-BENCHMARK/",
     "severity"  : "http://localhost:8300/severity"
+    // "severity"  : "AUDUT-SEVERITY/"
+    
 }
